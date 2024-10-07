e5dd8ccdcd1a9ee6c29ace6b4b7fbf4625cfa079
Dasarath G <dasarathg68@gmail.com>
2024-10-07 15:31:25 +0530

    refactor: update start command in backend/index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -12,15 +12,16 @@ if (!process.env.BOT_TOKEN) {
 const bot = new Bot(process.env.BOT_TOKEN); // <-- put your bot token between the ""
 
 bot.command("start", async (ctx) => {
-  const keyboard = new InlineKeyboard();
-  if (process.env.WEBAPP_URL)
-    keyboard.webApp("Open Instant Send App", process.env.WEBAPP_URL);
-
-  await ctx.reply("Press button bellow to open the App", {
-    reply_markup: keyboard,
-  });
-}); // Handle other messages.
-
+  if (process.env.WEBAPP_URL) {
+    let keyboard = new InlineKeyboard().webApp(
+      "Open Instant Send App",
+      process.env.WEBAPP_URL
+    );
+    await ctx.reply("Press the button below to open the App:", {
+      reply_markup: keyboard,
+    });
+  }
+});
 bot.on("message", (ctx) => {
   console.log(ctx.update.message.contact); // <-- log the contact object
   ctx.reply("Got another message!");
