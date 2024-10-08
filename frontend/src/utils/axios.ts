028fb6959ba5d7f34077427ae72875ada67fb65e
Dasarath G <dasarathg68@gmail.com>
2024-10-08 14:48:52 +0530

    refactor: Update production URL in axios configuration

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import data from "./urls.json";
 const instance = axios.create({
-  baseURL: data.development,
+  baseURL: data.production,
 });
 export default instance;
