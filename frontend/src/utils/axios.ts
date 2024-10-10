167547d5084065eb7623daf7317c999dc93702de
Dasarath G <dasarathg68@gmail.com>
2024-10-10 08:18:53 +0530

    Refactor: Update axios configuration for different environments

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
-import data from "./urls.json";
-const instance = axios.create({
-  baseURL: data.development,
-});
+
+const baseURL =
+  process.env.NEXT_PUBLIC_NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : process.env.NEXT_PUBLIC_API_URL;
+
+const instance = axios.create({ baseURL });
+
 export default instance;
